fae9624e0f571179f5549700cc21fb274d854478
Breno Teixeira <brenotx@Brenos-MacBook-Pro.local>
2019-06-01 23:41:55 -0400

    Handling big collections with pagination

--- test/read_test.js
+++ test/read_test.js
@@ -2,15 +2,17 @@ const assert = require("assert");
 const User = require("../src/user");
 
 describe("Reading user out of the database", () => {
-    let joe;
+    let joe, alex, maria, zach;
 
-    beforeEach(done => {
-        joe = new User({ name: "joe" });
-        joe.save().then(() => done());
+    beforeEach(async () => {
+        alex = await new User({ name: "Alex" }).save();
+        joe = await new User({ name: "Joe" }).save();
+        maria = await new User({ name: "Maria" }).save();
+        zach = await new User({ name: "Zach" }).save();
     });
 
     it("finds all users with the given name", done => {
-        User.find({ name: "joe" }).then(users => {
+        User.find({ name: "Joe" }).then(users => {
             assert(users[0]._id.toString === joe._id.toString);
             done();
         });
@@ -18,8 +20,18 @@ describe("Reading user out of the database", () => {
 
     it("find a user with a particular id", done => {
         User.findOne({ _id: joe._id }).then(user => {
-            assert(user.name === "joe");
+            assert(user.name === "Joe");
             done();
         });
+    });
+
+    it("can skip and limit the result set", async () => {
+        const users = await User.find({})
+            .sort({ name: 1 })
+            .skip(1)
+            .limit(2);
+        assert(users.length === 2);
+        assert(users[0].name === "Joe");
+        assert(users[1].name === "Maria");
     });
 });
